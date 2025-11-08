3bc80a6421591ba62a77f6f351d5f30c5474ccf7
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-11-08 21:17:41 +0530

    fonts and some tailwindConfigs added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       animation: {
+        'spin-slow': 'spin 10s linear infinite',
+        'spin-slow-reverse': 'spin-reverse 10s linear infinite',
+      },
+      fontFamily: {
+        baseText : ['"Instrument Sans"'],
+        headingText : ['"Island Moments"'],
+        mediumText : ['"Halant"']
+      },
+      colors: {
+        primary : "#f25d2c"
+      }
+    }
   },
   plugins: [],
 }
