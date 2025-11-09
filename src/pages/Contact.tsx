edaab82e2e725c0dfba9199978f469957453e5bc
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-11-09 17:07:37 +0530

    contactPage-underDevelopment

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,9 @@
+export default function ContactPage(){
+    return ( 
+    <div className="w-screen h-screen flex items-center justify-center ">
+        <p className="font-headingText text-[1rem] border border-solid border-black">
+            CONTACT PAGE UNDER DEVELOPMENT
+        </p>
+    </div>
+    )
+}
