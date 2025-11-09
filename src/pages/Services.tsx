ab9f077e49dad49c64b82050a1d816be0b41bc14
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-11-09 17:14:49 +0530

    servicePage underDevelopment

---
+++ src/pages/Services.tsx
@@ -0,0 +1,9 @@
+export default function ServicesPage(){
+    return ( 
+    <div className="w-screen h-screen flex items-center justify-center ">
+        <p className="font-headingText text-[1rem]">
+            SERVICES UNDER DEVELOPMENT
+        </p>
+    </div>
+    )
+}
